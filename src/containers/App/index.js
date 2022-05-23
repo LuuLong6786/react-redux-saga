6f3ca53f70d6799dd1258e48f891d8836842cf8c
Long Luu Tu <ltlong@tma.com.vn>
2022-05-23 17:49:14 +0700

    first commit

---
+++ src/containers/App/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Button from "@mui/icons-material";
+import { ThemeProvider } from "@emotion/react";
+import styles from "../App/styles";
+import { withStyles } from "@mui/material";
+
+function App() {
+  return <div>Hello</div>;
+}
+export default withStyles(styles)(App);
